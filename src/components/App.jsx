1ddb7c558009f73ad88f6f629e9248c517137398
NaraeJang <j.naaaarae@gmail.com>
2023-03-13 16:48:29 -0400

    Mapping Data to component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Heading from "./Header.jsx";
 import Footer from "./Footer.jsx";
-import Note from "./Note.jsx";
+import NoteTemplate from "./Note.jsx";
+import notes from "../note.js";
+
+
 
 function App() {
     return (
     <div>
         <Heading />
-        <Note />
+        {notes.map(note => <NoteTemplate 
+            key={note.id}
+            title={note.title}
+            content={note.content}
+        />)}
         <Footer />
     </div>
     );
