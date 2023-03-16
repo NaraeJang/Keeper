39d540b53cdbb4690cf0f2c9c1c1539a5726fc0f
NaraeJang <j.naaaarae@gmail.com>
2023-03-16 16:18:31 -0400

    Set up for part4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 
-
 function Footer() {
-
-    const d = new Date();
-    const currentYear = d.getFullYear();
-
-    return (
-<footer>
-    <p>Copyright © {currentYear}</p>
-</footer>
-    );
-};
-
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
+}
 
 export default Footer;
