7dd929df8f241dcbb254c09bfa4b2574971420da
NaraeJang <j.naaaarae@gmail.com>
2023-03-16 11:40:26 -0400

    Add function and optimize the steps.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateArea() {
+  // const [contentText, setContentText] = useState("");
+  // const [titleText, setTitleText] = useState("");
+
+  // function handleContentText(event) {
+  //   const newContent = event.target.value;
+  //   setContentText(newContent);
+  // }
+
+  // function handleTitleText(event) {
+  //   const newTitle = event.target.value;
+  //   setTitleText(newTitle);
+  // }
+
+  const [text, setText] = useState({ title: "", content: "" });
+
+  function handleText(event) {
+    const { name, value } = event.target;
+
+    setText((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input
+          onChange={handleText}
+          name="title"
+          placeholder="Title"
+          value={text.title}
+        />
+        <textarea
+          onChange={handleText}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={text.content}
+        />
         <button>Add</button>
       </form>
     </div>
